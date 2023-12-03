b9d2859d45d2efdff13d962f85b463f90c8a5c61
Guarlic <hyunujung0419@gmail.com>
2023-12-03 10:09:17 +0900

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use bevy::app::AppExit;
 use bevy::prelude::*;
 use bevy::sprite::collide_aabb::collide;
+use bevy::window::WindowTheme;
 use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup_camera, setup_player, setup_enemy))
-        .add_systems(Update, (player_move, enemy_move, game_over_collision))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Test".into(),
+                resolution: (1000., 700.).into(),
+                window_theme: Some(WindowTheme::Dark),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, (setup_camera, setup_player, setup_timer))
+        .add_systems(
+            Update,
+            (player_move, update_enemy, enemy_move, game_over_collision),
+        )
         .run();
 }
 
@@ -19,6 +32,9 @@ struct Player;
 struct Enemy;
 
 #[derive(Component)]
+struct TimerStruct(Timer);
+
+#[derive(Component)]
 enum Direction {
     Up,
     Down,
@@ -26,6 +42,8 @@ enum Direction {
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+
+    sleep(Duration::from_secs(1));
 }
 
 fn setup_player(mut commands: Commands) {
@@ -42,34 +60,8 @@ fn setup_player(mut commands: Commands) {
     ));
 }
 
-fn setup_enemy(mut commands: Commands) {
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(100., 30.)),
-                color: Color::rgb(255., 255., 0.),
-                ..default()
-            },
-            transform: Transform::from_xyz(100., 2., 0.),
-            ..default()
-        },
-        Direction::Up,
-        Enemy,
-    ));
-
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(100., 30.)),
-                color: Color::rgb(255., 255., 0.),
-                ..default()
-            },
-            transform: Transform::from_xyz(-100., 100., 0.),
-            ..default()
-        },
-        Direction::Down,
-        Enemy,
-    ));
+fn setup_timer(mut commands: Commands) {
+    commands.spawn(TimerStruct(Timer::from_seconds(5., TimerMode::Repeating)));
 }
 
 fn player_move(
@@ -91,6 +83,38 @@ fn player_move(
                 let distance = PLAYER_SPEED * time.delta_seconds();
 
                 player_transform.translation += *direction * distance;
+
+                player_transform.translation.x = player_transform.translation.x.clamp(-450., 450.);
+                player_transform.translation.y = player_transform.translation.y.clamp(-300., 300.);
+            }
+        }
+    }
+}
+
+fn update_enemy(
+    time: Res<Time>,
+    mut timer_query: Query<&mut TimerStruct>,
+    player_query: Query<&Transform, With<Player>>,
+    mut commands: Commands,
+) {
+    for mut timer in timer_query.iter_mut() {
+        if timer.0.tick(time.delta()).just_finished() {
+            for player_transform in player_query.iter() {
+                let player_x = player_transform.translation.x;
+
+                commands.spawn((
+                    SpriteBundle {
+                        sprite: Sprite {
+                            custom_size: Some(Vec2::new(100., 30.)),
+                            color: Color::rgb(255., 255., 0.),
+                            ..default()
+                        },
+                        transform: Transform::from_xyz(player_x, 150., 0.),
+                        ..default()
+                    },
+                    Direction::Down,
+                    Enemy,
+                ));
             }
         }
     }
@@ -109,9 +133,9 @@ fn enemy_move(
             Direction::Down => enemy_transform.translation.y -= distance,
         }
 
-        if enemy_transform.translation.y > 200. {
+        if enemy_transform.translation.y > 300. {
             *enemy_direction = Direction::Down;
-        } else if enemy_transform.translation.y < -200. {
+        } else if enemy_transform.translation.y < -300. {
             *enemy_direction = Direction::Up;
         }
     }
@@ -132,7 +156,6 @@ fn game_over_collision(
             );
 
             if let Some(_) = collision {
-                println!("Game Over!");
                 sleep(Duration::from_secs(1));
 
                 exit_events.send(AppExit);
